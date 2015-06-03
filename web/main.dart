395c0b3fe18e780f7ca42b585af07bb9dd8b72a8
Jonas Kello <jonas.kello@divid.se>
2015-06-03 14:30:20 +0200

    Cleanup

--- web/main.dart
+++ web/main.dart
@@ -7,20 +7,17 @@ import 'package:react/react_client.dart' as reactClient;
 import 'package:react_bootstrap/react_bootstrap.dart' as react_bootstrap;
 
 main() {
-
   reactClient.setClientConfiguration();
 
 //  var component = div({}, "Hello world!");
 //  render(component, querySelector('#content'));
 
   render(rootComponent({}), querySelector('#content'));
-
 }
 
 var rootComponent = registerComponent(() => new RootComponent());
 
 class RootComponent extends Component {
-
   var colors = ['orange', 'red', 'blue', 'purple'];
 
 //  getInitialState() {
@@ -33,32 +30,47 @@ class RootComponent extends Component {
 //    };
 //  }
 
-  render() =>
-  div({}, [
-    h1({"style": {"color": "red"}}, "DEMO"),
-    div({"style": {"padding": "10px"}}, [
-      this.state["value"],
-      react_bootstrap.Alert({
-        'bsStyle': 'warning',
-      }, div({}, "HELLO!")),
-    ]),
-    div({"style": {"padding": "10px"}}, [
-      this.state["value"],
-      react_bootstrap.Button({
-        'bsStyle': 'primary',
-      }, "Primary"),
-    ]),
-    /*
-    div({"style": {"padding": "10px"}}, [
-      "NumberPicker: ${this.state["numberPickerValue"]}",
-      widgets.numberPicker({
-        "style": {"width": "200px"},
-        "format": (v, c) => v,
-        "parse": (v, c) => v,
-        "value": this.state["numberPickerValue"],
-        "onChange": (value) => this.setState({"numberPickerValue": value})}),
-    ]),
-    */
-  ]);
+  render() => div({}, [
+    h1({"style": {"color": "red"}}, [ "DEMO"]),
 
+
+    div({
+      "style": {"padding": "10px"}
+    }, [react_bootstrap.Alert({'bsStyle': 'warning', }, div({}, "HELLO!")), ]),
+    div({
+      "style": {"padding": "10px"}
+    }, [react_bootstrap.Button({'bsStyle': 'primary', }, 'Primary')]),
+
+
+/*
+<ButtonToolbar>
+<ButtonGroup>
+<Button><Glyphicon glyph='align-left' /></Button>
+<Button><Glyphicon glyph='align-center' /></Button>
+<Button><Glyphicon glyph='align-right' /></Button>
+<Button><Glyphicon glyph='align-justify' /></Button>
+</ButtonGroup>
+</ButtonToolbar>
+*/
+
+
+    div({
+      "style": {"padding": "10px"}
+    },
+    react_bootstrap.ButtonToolbar({},
+    react_bootstrap.ButtonGroup({}, [
+      react_bootstrap.Button({}, react_bootstrap.Glyphicon({'glyph': 'align-left'})),
+      react_bootstrap.Button({}, react_bootstrap.Glyphicon({'glyph': 'align-center'})),
+      react_bootstrap.Button({}, react_bootstrap.Glyphicon({'glyph': 'align-right'})),
+      react_bootstrap.Button({}, react_bootstrap.Glyphicon({'glyph': 'align-justify'})),
+    ]))),
+
+    div({"style": {"padding": "10px"}}, [
+      react_bootstrap.Button(
+          {}, react_bootstrap.Glyphicon({'glyph': 'star', }, 'Star')),
+    ]),
+
+
+
+  ]);
 }
