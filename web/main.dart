5ea21db49a194b52c93a4cde752bbeb83162acff
Jonas Kello <jonas.kello@divid.se>
2015-06-03 12:54:12 +0200

    Initial commit

---
+++ web/main.dart
@@ -0,0 +1,97 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'dart:html';
+import 'package:react/react.dart';
+import 'package:react/react_client.dart' as reactClient;
+import 'package:react_bootstrap/react_bootstrap.dart' as react_bootstrap;
+
+main() {
+
+  reactClient.setClientConfiguration();
+
+  var component = div({}, "Hello world!");
+  render(component, querySelector('#content'));
+
+//  render(rootComponent({}), querySelector('#content'));
+
+}
+
+var rootComponent = registerComponent(() => new RootComponent());
+
+class RootComponent extends Component {
+
+  var colors = ['orange', 'red', 'blue', 'purple'];
+
+  getInitialState() {
+    return {
+      "dropdownListValue": colors[0],
+      "comboboxValue": colors[0],
+      "numberPickerValue": 5,
+      "multiselectValue": [colors[0], colors[2]],
+      "selectListValue": [colors[0]],
+    };
+  }
+
+  render() =>
+  div({}, [
+    h1({"style": {"color": "red"}}, "DEMO"),
+    div({"style": {"padding": "10px"}}, [
+      "DropdownList: ${this.state["dropdownListValue"]}",
+      this.state["value"],
+      react_bootstrap.dropdownList({
+        "style": {"width": "200px"},
+        "data": colors,
+        "value": this.state["dropdownListValue"],
+        "onChange": (value) => this.setState({"dropdownListValue": value})
+      }),
+    ]),
+    div({"style": {"padding": "10px"}}, [
+      "Combobox: ${this.state["comboboxValue"]}",
+      react_bootstrap.combobox({
+        "style": {"width": "200px"},
+        "data": colors,
+        "value": this.state["comboboxValue"],
+        "onChange": (value) => this.setState({"comboboxValue": value})}),
+    ]),
+    /*
+    div({"style": {"padding": "10px"}}, [
+      "NumberPicker: ${this.state["numberPickerValue"]}",
+      widgets.numberPicker({
+        "style": {"width": "200px"},
+        "format": (v, c) => v,
+        "parse": (v, c) => v,
+        "value": this.state["numberPickerValue"],
+        "onChange": (value) => this.setState({"numberPickerValue": value})}),
+    ]),
+    */
+    div({"style": {"padding": "10px"}}, [
+      "Multiselect: ${this.state["multiselectValue"]}",
+      react_bootstrap.multiselect({
+        "style": {"width": "200px"},
+        "data": colors,
+        "value": this.state["multiselectValue"],
+        "onChange": (value) => this.setState({"multiselectValue": value})}),
+    ]),
+    div({"style": {"padding": "10px"}}, [
+      "SelectList: ${this.state["selectListValue"]}",
+      react_bootstrap.selectList({
+        "style": {"width": "200px"},
+        "data": colors,
+        "value": this.state["selectListValue"],
+        "onChange": (value) => this.setState({"selectListValue": value})}),
+    ]),
+    /*
+    div({"style": {"padding": "10px"}}, [
+      "Calendar: ${this.state["calendarValue"]}",
+      widgets.calendar({
+        "style": {"width": "200px"},
+//        "headerFormat": (v, c) => v,
+        "parse": (v, c) => v,
+        "value": this.state["calendarValue"],
+        "onChange": (value) => this.setState({"calendarValue": value})}),
+    ]),
+        */
+  ]);
+
+}
