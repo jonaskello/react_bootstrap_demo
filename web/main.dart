4a8e7a6583573550d293f7d6b7b973bfe686febc
Jonas Kello <jonas.kello@divid.se>
2015-06-03 15:18:03 +0200

    Nav demo

--- web/main.dart
+++ web/main.dart
@@ -20,70 +20,43 @@ var rootComponent = registerComponent(() => new RootComponent());
 class RootComponent extends Component {
   var colors = ['orange', 'red', 'blue', 'purple'];
 
-//  getInitialState() {
-//    return {
-//      "dropdownListValue": colors[0],
-//      "comboboxValue": colors[0],
-//      "numberPickerValue": 5,
-//      "multiselectValue": [colors[0], colors[2]],
-//      "selectListValue": [colors[0]],
-//    };
-//  }
-
+  getInitialState() {
+    return {
+      'activeKey': '2',
+    };
+  }
 
   void handleSelect(selectedKey, [b, c]) {
-    window.alert('selected ' + selectedKey);
+    print('selected ' + selectedKey);
+    this.setState({'activeKey': selectedKey });
   }
 
   render() => div({}, [
-    h1({"style": {"color": "red"}}, [ "DEMO"]),
+    h1({'key': 1, 'style': {'color': 'red'}}, 'DEMO'),
+    div({ 'key': 2, 'style': {'padding': '10px'} }, bs.Alert({'bsStyle': 'warning', }, div({}, "HELLO!"))),
+    div({ 'key': 3, 'style': {'padding': '10px'} }, bs.Button({'bsStyle': 'primary', }, 'Primary')),
 
 
-    div({
-      "style": {"padding": "10px"}
-    }, [bs.Alert({'bsStyle': 'warning', }, div({}, "HELLO!")), ]),
-    div({
-      "style": {"padding": "10px"}
-    }, [bs.Button({'bsStyle': 'primary', }, 'Primary')]),
-
-
-/*
-<ButtonToolbar>
-<ButtonGroup>
-<Button><Glyphicon glyph='align-left' /></Button>
-<Button><Glyphicon glyph='align-center' /></Button>
-<Button><Glyphicon glyph='align-right' /></Button>
-<Button><Glyphicon glyph='align-justify' /></Button>
-</ButtonGroup>
-</ButtonToolbar>
-*/
-
-
-    div({
-      "style": {"padding": "10px"}
-    },
+    div({ 'key': 4, 'style': {'padding': '10px'} },
     bs.ButtonToolbar({},
     bs.ButtonGroup({}, [
-      bs.Button({}, bs.Glyphicon({'glyph': 'align-left'})),
-      bs.Button({}, bs.Glyphicon({'glyph': 'align-center'})),
-      bs.Button({}, bs.Glyphicon({'glyph': 'align-right'})),
-      bs.Button({}, bs.Glyphicon({'glyph': 'align-justify'})),
+      bs.Button({'key': 1}, bs.Glyphicon({'glyph': 'align-left'})),
+      bs.Button({'key': 2}, bs.Glyphicon({'glyph': 'align-center'})),
+      bs.Button({'key': 3}, bs.Glyphicon({'glyph': 'align-right'})),
+      bs.Button({'key': 4}, bs.Glyphicon({'glyph': 'align-justify'})),
     ]))),
-    div({"style": {"padding": "10px"}}, [
-      bs.Button(
-          {}, bs.Glyphicon({'glyph': 'star', }, 'Star')),
-    ]),
+    div({'key': 5, 'style': {'padding': '10px'}}, bs.Button({}, bs.Glyphicon({'glyph': 'star', }, 'Star'))),
 
-    bs.Nav({'bsStyle': 'tabs', 'activeKey': 1, 'onSelect': this.handleSelect}, [
-      bs.NavItem({'eventKey': '1', 'href': '/home'}, 'NavItem 1 content'),
-      bs.NavItem({'eventKey': '2', 'title': 'Item', }, 'NavItem 2 content'),
-      bs.NavItem({'eventKey': '3', 'disabled': true}, 'NavItem 3 content'),
-      bs.DropdownButton({'eventKey': '4', 'title': 'Dropdown', 'navItem': true, 'onSelect': (a) => this.handleSelect(a, null, null)}, [
-        bs.MenuItem({'eventKey': '4.1'}, 'Action'),
-        bs.MenuItem({'eventKey': '4.2'}, 'Another action'),
-        bs.MenuItem({'eventKey': '4.3'}, 'Something else here'),
-        bs.MenuItem({'divider': true}),
-        bs.MenuItem({'eventKey': '4.4'}, 'Separated link')
+    bs.Nav({'key': 6, 'bsStyle': 'tabs', 'activeKey': this.state['activeKey'], 'onSelect': this.handleSelect}, [
+      bs.NavItem({'key': 1, 'eventKey': '1', 'href': '/home'}, 'NavItem 1 content'),
+      bs.NavItem({'key': 2, 'eventKey': '2', 'title': 'Item', }, 'NavItem 2 content'),
+      bs.NavItem({'key': 3, 'eventKey': '3', 'disabled': true}, 'NavItem 3 content'),
+      bs.DropdownButton({'key': 4, 'eventKey': '4', 'title': 'Dropdown', 'navItem': true, 'onSelect': (a) => this.handleSelect(a, null, null)}, [
+        bs.MenuItem({'key': 1, 'eventKey': '4.1'}, 'Action'),
+        bs.MenuItem({'key': 2, 'eventKey': '4.2'}, 'Another action'),
+        bs.MenuItem({'key': 3, 'eventKey': '4.3'}, 'Something else here'),
+        bs.MenuItem({'key': 4, 'divider': true}),
+        bs.MenuItem({'key': 5, 'eventKey': '4.4'}, 'Separated link')
       ])]
     )
 
